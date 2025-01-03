b412d6fc3fc9a20389a2dcfc332c9db999b57e90
Chandra Sekhar Mondeti <chandra.sekhar0714@gmail.com>
2025-01-03 19:58:26 +0530

    Update test_example.py

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,12 +1,18 @@
 # test_sample.py
 
 def test_sum():
+     """
+    Test case to validate the sum of a number and 5.
+    """
     a = 5  # Unused variable
     b = 10
     result = b + 5
     assert result == 15
 
 def test_product():
+    """
+    Test case to validate the product of a number and 2.
+    """
     x = 2  # Unused variable
     y = 3
     product = y * 2
