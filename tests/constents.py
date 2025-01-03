972fb072ea4674798079d570c44763bede5d4a30
Chandra Sekhar Mondeti <chandra.sekhar0714@gmail.com>
2025-01-03 20:33:43 +0530

    Update constents.py

--- tests/constents.py
+++ tests/constents.py
@@ -1,3 +1,9 @@
+"""
+This module contains constants used across the test suite.
+
+These constants are intended to provide reusable values
+to ensure consistency in test cases.
+"""
 # ----- SELECTORS CONSTANTS ----- #
 LOGIN_BUTTON = '#loginButton'
 PASSWORD_INPUT = '#password'
